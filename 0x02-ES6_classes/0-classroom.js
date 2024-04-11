a7c104095a9862c9123338fb2071ad1ee7bcf977
Hisham Balboul <hishamsabry_97@hotmail.com>
2024-04-11 17:29:25 +0000

    Task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new ClassRoom.
+   * maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
